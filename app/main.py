14af4bbd2a9c2c1e8f28ecd5e2ffa4b46d655880
Betsinat <Betsinat39@gmail.com>
2026-07-03 17:50:21 +0300

    Register users router in fastapi app

--- app/main.py
+++ app/main.py
@@ -1,24 +1,6 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from app.users.routes import router as users_router
 
 app = FastAPI()
-class UserCreate(BaseModel):
-    username : str
-    email: str
-    password : str
 
-@app.get("/")
-def home():
-    return {"message": "Welcome to Mini social platform API"}
-
-@app.get("/health")
-def health():
-    return {"status" : "healthy"}
-
-@app.post("/register")
-def register(user:UserCreate):
-    return{
-        "message": "User recieved successfully",
-        "username": user.username
-
-    }
+app.include_router(users_router)
