4c593f35f66082e372b03584f2f96d90d2a2e0d3
Betsinat <Betsinat39@gmail.com>
2026-07-03 18:33:51 +0300

    Register posts router in FastAPI app

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from users.routes import router as users_router
+from posts.routes import router as posts_router
 
 app = FastAPI()
 
 app.include_router(users_router)
+app.include_router(posts_router)
