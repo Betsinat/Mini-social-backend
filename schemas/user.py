3571152081d8fe548ae7a994369c8c3417ec8196
Betsinat <Betsinat39@gmail.com>
2026-08-05 07:48:22 +0300

    update user schema

--- schemas/user.py
+++ schemas/user.py
@@ -1,6 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel , EmailStr
 
 class userCreate(BaseModel):
     username : str
     email: str
     age: str
+
+#what the server responds
+class userResponse(BaseModel):
+    id: int
+    username:str
+    email:str
+
+    class Config:
+        from_attributes = True
