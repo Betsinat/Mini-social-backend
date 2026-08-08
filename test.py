12b7a1a55914d44919d70356110468d6d043156d
Betsinat <Betsinat39@gmail.com>
2026-08-08 20:04:25 +0300

    feat(auth): replace password hashing tests with JWT token generation and decoding

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
-from auth.hash import hash_password, verify_password
+import os
+from dotenv import load_dotenv
+import jwt
+from auth.jwt import create_access_token
 
-# Test password
-my_password = "securepassword123"
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
 
-# 1. Hash the same password twice
-hash_one = hash_password(my_password)
-hash_two = hash_password(my_password)
+token = create_access_token(data={"sub": "1"})
+print(f"Generated Token: {token}\n")
 
-print(f"Hash 1: {hash_one}")
-print(f"Hash 2: {hash_two}")
+parts = token.split(".")
+print(f"Number of parts in JWT: {len(parts)} (Should be 3)")
 
-print(f"Are the hashes identical? {hash_one == hash_two}")
-
-print(f"Verify hash 1: {verify_password(my_password, hash_one)}")
-print(f"Verify hash 2: {verify_password(my_password, hash_two)}")
+decoded_data = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+print(f"Decoded Data: {decoded_data}")
