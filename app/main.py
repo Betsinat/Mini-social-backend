73a0dfadb9195a1c85565d0fa7d5354cee710cdc
Betsinat <Betsinat39@gmail.com>
2026-08-09 13:50:11 +0300

    feat(auth): add login endpoint for JWT access token generation

--- app/main.py
+++ app/main.py
@@ -2,7 +2,10 @@ from fastapi import FastAPI
 from users.routes import router as users_router
 from posts.routes import router as posts_router
 from auth.routes import router as auth_router
+from db.models import Base
+from db.database import engine
 
+Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(users_router)
--- auth/routes.py
+++ auth/routes.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter , Depends , HTTPException , status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
 from db.database import get_db
 from db.models import User
 from schemas.user import userCreate, userResponse
-from auth.hash import hash_password
+from auth.hash import hash_password , verify_password
+from auth.jwt import create_access_token
 
 router = APIRouter(prefix = "/auth" , tags =["Auth"])
 
@@ -29,6 +31,28 @@ def register_user(user: userCreate , db:Session = Depends(get_db)):
             detail = "username or email already registered"
         )
     return new_user
+
+@router.post("/login")
+def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(), 
+    db: Session = Depends(get_db)
+):
+    user = db.query(User).filter(User.username == form_data.username).first()
+    
+    if not user or not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+        
+    access_token = create_access_token(data={"sub": str(user.id)})
+    
+  
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
 
 
    
